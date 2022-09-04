c3f1dc1f4e7695fce97307503c41f05d3a80af88
LimHun <dlagnskkld3@gmail.com>
2022-09-05 01:05:18 +0900

    수정

---
+++ SwiftUIPractice/StickyHeader/View/StickyTopBar.swift
@@ -0,0 +1,43 @@
+//
+//  SwiftUIView.swift
+//  SwiftUIPractice
+//
+//  Created by tunko on 2022/09/04.
+//
+
+import SwiftUI
+
+struct StickyTopBar : View {
+
+    var topEdge : CGFloat
+    @Binding var offset : CGFloat
+    var maxHeight: CGFloat
+
+    var body: some View {
+
+        VStack(alignment: .leading, spacing: 15) {
+
+            Image("Hun")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 80, height: 80)
+                .cornerRadius(10)
+
+            Text("Lim Hun")
+                .font(.largeTitle.bold())
+
+            Text("texteworjweorjiweorijweorijweorijweoriwjeroijweroiwjeroiwjreowierjwoeirjwoerijweoirjweorijwoerijwoeirj")
+                .fontWeight(.semibold)
+                .foregroundColor(.white.opacity(0.8))
+        }
+        .padding()
+        .padding(.bottom)
+        .opacity(getOpacity())
+    }
+
+    func getOpacity() -> CGFloat {
+        let progress = -offset / 70
+        let opacity = 1 - progress
+        return offset < 0 ? opacity : 1
+    }
+}
