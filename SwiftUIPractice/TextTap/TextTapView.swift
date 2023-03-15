3833b7eb35b03c98499ffdbf1de52a0d53a238e3
LimHun <dlagnskkld3@gmail.com>
2023-03-16 00:32:30 +0900

    TextTapView

---
+++ SwiftUIPractice/TextTap/TextTapView.swift
@@ -0,0 +1,20 @@
+//
+//  TextTapView.swift
+//  SwiftUIPractice
+//
+//  Created by 임훈 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct TextTapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextTapView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextTapView()
+    }
+}
