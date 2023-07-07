380f0231015429ffd475475c8f527ce4aa213515
LimHun <dlagnskkld3@gmail.com>
2023-07-08 00:02:43 +0900

    MovableGridsView
    그리드뷰에서 드래그엔 드랍으로 아이템 이동 및 순서 조정

---
+++ SwiftUIPractice/MovableGrids/MovableGridsView.swift
@@ -0,0 +1,20 @@
+//
+//  MovableGridsView.swift
+//  SwiftUIPractice
+//
+//  Created by 임훈 on 2023/07/07.
+//
+
+import SwiftUI
+
+struct MovableGridsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovableGridsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovableGridsView()
+    }
+}
