8cd6f9ab0b3bdbdc86664a038a7b5659eca8499b
tunko <tunko@kyobobook.com>
2023-07-09 16:49:09 +0900

    InfiniteCarousel
    무한 스크롤링 TabView

---
+++ SwiftUIPractice/InfiniteCarousel/Model/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  SwiftUIPractice
+//
+//  Created by 임훈 on 2023/07/09.
+//
+
+import SwiftUI
+
+struct PageModel: Identifiable, Hashable {
+    var id: UUID = .init()
+    var color: Color
+}
