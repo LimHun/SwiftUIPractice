72ed2dba31bf09a924390ee6c6a7b05391621047
임훈 <tunko@kyobobook.com>
2023-08-06 16:39:13 +0900

    리베이스 테스트

---
+++ test/RebaseTest.swift
@@ -0,0 +1,20 @@
+//
+//  RebaseTest.swift
+//  SwiftUIPractice
+//
+//  Created by tunko on 2023/08/06.
+//
+
+import SwiftUI
+
+struct RebaseTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RebaseTest_Previews: PreviewProvider {
+    static var previews: some View {
+        RebaseTest()
+    }
+}
